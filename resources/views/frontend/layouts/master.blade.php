98af6bc9a48aac4a4ac147dd80fe0dce2f2263cf
M Ali Weber <127172065+Ashaliwk@users.noreply.github.com>
2026-09-26 17:13:13 +0500

    updated laravel project

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -27,97 +27,76 @@
 <body>
   <nav class="navbar navbar-expand-xl fixed-top site-nav">
     <div class="container-fluid px-3 px-xl-5">
-
-        <a class="navbar-brand" href="{{ url('/') }}">
-
-            <img
-                src="{{ asset('assets/images/gmet-logo.png') }}"
-                alt="GMET Logo"
-                class="brand-logo"
-            >
-
-            <span class="brand-text">
-                GME <b>TECHNOLOGIES</b>
-                <small>Geo Mapping Engineering & Technologies</small>
-            </span>
-
-        </a>
-
-        <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#mainNav"
-            aria-controls="mainNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="mainNav">
-
-            <ul class="navbar-nav ms-auto align-items-xl-center">
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('/') ? 'active' : '' }}"
-                       href="{{ url('/') }}">Home</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('about*') ? 'active' : '' }}"
-                       href="{{ url('/about') }}">About</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('services*') ? 'active' : '' }}"
-                       href="{{ url('/services') }}">Services</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('team*') ? 'active' : '' }}"
-                       href="{{ url('/team') }}">Team</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('projects*') ? 'active' : '' }}"
-                       href="{{ url('/projects') }}">Projects</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('partners*') ? 'active' : '' }}"
-                       href="{{ url('/partners') }}">Partners</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('blog*') ? 'active' : '' }}"
-                       href="{{ url('/blog') }}">Blog</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('resources*') ? 'active' : '' }}"
-                       href="{{ url('/resources') }}">Resources</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link {{ Request::is('contact*') ? 'active' : '' }}"
-                       href="{{ url('/contact') }}">Contact</a>
-                </li>
-
-                <li class="nav-item ms-xl-2 mt-2 mt-xl-0">
-                    <button
-                        id="themeToggle"
-                        class="btn theme-toggle"
-                        type="button">
-                        ☾
-                    </button>
-                </li>
-
-            </ul>
-
-        </div>
+      <a class="navbar-brand" href="{{ url('/') }}">
+        <img
+          src="{{ asset('assets/images/gmet-logo.png') }}"
+          alt="GMET Logo"
+          class="brand-logo">
+        <span class="brand-text">
+          GME <b>TECHNOLOGIES</b>
+          <small>Geo Mapping Engineering & Technologies</small>
+        </span>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#mainNav"
+        aria-controls="mainNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="mainNav">
+        <ul class="navbar-nav ms-auto align-items-xl-center">
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('/') ? 'active' : '' }}"
+              href="{{ url('/') }}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('about*') ? 'active' : '' }}"
+              href="{{ url('/about') }}">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('services*') ? 'active' : '' }}"
+              href="{{ url('/services') }}">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('team*') ? 'active' : '' }}"
+              href="{{ url('/team') }}">Team</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('projects*') ? 'active' : '' }}"
+              href="{{ url('/projects') }}">Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('partners*') ? 'active' : '' }}"
+              href="{{ url('/partners') }}">Partners</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('blog*') ? 'active' : '' }}"
+              href="{{ url('/blog') }}">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('resources*') ? 'active' : '' }}"
+              href="{{ url('/resources') }}">Resources</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{ Request::is('contact*') ? 'active' : '' }}"
+              href="{{ url('/contact') }}">Contact</a>
+          </li>
+          <li class="nav-item ms-xl-2 mt-2 mt-xl-0">
+            <button
+              id="themeToggle"
+              class="btn theme-toggle"
+              type="button">
+              ☾
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
-</nav>
+  </nav>
 
   @yield('content')
   <footer class="site-footer">
